f800a3722669eca64c69d60afe8de021fb77f601
zding5 <zding5@illinois.edu>
2015-11-10 19:26:21 -0600

    crn_validator updated

--- crn_validator.py
+++ crn_validator.py
@@ -10,7 +10,7 @@ import sys
 # sys.stdout = open("validator_log.txt", 'w')
 # sys.stderr = open("validator_error.txt", 'w')
 
-base_url = "https://ui2web1.apps.uillinois.edu/BANPROD1/bwckschd.p_disp_detail_sched?term_in=120158&crn_in="
+base_url = "https://ui2web1.apps.uillinois.edu/BANPROD1/bwckschd.p_disp_detail_sched?term_in=120161&crn_in="
 
 
 def initBrowser():
